3de73b4c11875ad32b43d9bb05f0083da8332d8f
Hannah Nguyen <myhanh.nguyen.969317598@gmail.com>
2024-03-28 09:19:01 +0200

    add express-rate-limit to limit repeated requests

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import { config } from 'dotenv';
 import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
 
 config();
 
@@ -10,9 +11,17 @@ const PORT = process.env.PORT || 8000;
 const app: express.Application = express();
 
 //middleware
-app.use(express.json());
-app.use(cors());
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 10 minutes).
+  standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+app.use(limiter);
+
 app.use(helmet());
+app.use(cors());
+app.use(express.json());
 
 //listeners
 app.listen(PORT, () => {
