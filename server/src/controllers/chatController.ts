16cd55cee2145663a43095caf71acab6f97a4b78
Hannah Nguyen <myhanh.nguyen.969317598@gmail.com>
2024-04-01 16:59:13 +0300

    add get all chats logic, add delete chats route

--- server/src/controllers/chatController.ts
+++ server/src/controllers/chatController.ts
@@ -30,18 +30,64 @@ export const generateChatCompletionController = async (
     user.chats.push({ role: 'user', content: message });
 
     // send all chats  with the new one to openAI API
-    const openAi = new OpenAIApi(configurationOpenAI);
+    const openAi = new OpenAIApi(configurationOpenAI());
 
     const chatResponse = await openAi.createChatCompletion({
       model: 'gpt-3.5-turbo',
       messages: updatedChats,
     });
-
+    console.log('res ai', chatResponse.data.choices[0].message);
     user.chats.push(chatResponse.data.choices[0].message);
     await user.save();
     return res
       .status(200)
       .json({ message: 'Chat completion was successfully', chats: user.chats });
+  } catch (err) {
+    // console.log(err);
+    return res.status(404).json({ message: 'error', error: err });
+  }
+};
+
+export const getAllChatsController = async (req: Request, res: Response) => {
+  try {
+    //check if the access token is valid
+    const user = await User.findById(res.locals.jwtData.userId);
+    if (!user) {
+      return res
+        .status(401)
+        .send('User not registered OR access token malfunctioned');
+    }
+
+    if (user._id.toString() !== res.locals.jwtData.userId) {
+      return res.status(401).send("Permissions didn't match");
+    }
+
+    return res
+      .status(200)
+      .json({ message: 'get all chats successfully', chats: user.chats });
+  } catch (err) {
+    console.log(err);
+    return res.status(404).json({ message: 'error', error: err });
+  }
+};
+
+export const deleteChatsController = async (req: Request, res: Response) => {
+  try {
+    //check if the access token is valid
+    const user = await User.findById(res.locals.jwtData.userId);
+    if (!user) {
+      return res
+        .status(401)
+        .send('User not registered OR access token malfunctioned');
+    }
+
+    if (user._id.toString() !== res.locals.jwtData.userId) {
+      return res.status(401).send("Permissions didn't match");
+    }
+    //@ts-ignore
+    user.chats = [];
+    await user.save();
+    return res.status(200).json({ message: 'delete all chats successfully' });
   } catch (err) {
     console.log(err);
     return res.status(404).json({ message: 'error', error: err });
--- server/src/routes/chatRoutes.ts
+++ server/src/routes/chatRoutes.ts
@@ -1,14 +1,22 @@
 import express from 'express';
 import { chatCompletionValidator, validate } from '../utils/validator.js';
 import { verifyAccessToken } from '../utils/token.js';
-import { generateChatCompletionController } from '../controllers/chatController.js';
+import {
+  deleteChatsController,
+  generateChatCompletionController,
+  getAllChatsController,
+} from '../controllers/chatController.js';
 
 const chatRoutes = express.Router();
+
 chatRoutes.post(
   '/new',
   validate(chatCompletionValidator),
   verifyAccessToken,
   generateChatCompletionController
 );
+
+chatRoutes.get('/all-chats', verifyAccessToken, getAllChatsController);
+chatRoutes.delete('/delete', verifyAccessToken, deleteChatsController);
 
 export default chatRoutes;
