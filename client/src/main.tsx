56335b9d2f2b3f5f7712578eb0b6f6ffe5579f4e
Hannah Nguyen <myhanh.nguyen.969317598@gmail.com>
2024-04-01 20:45:50 +0300

    add axios package, add routes with react-router-dom

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
 import { createTheme, ThemeProvider } from '@mui/material';
+import { BrowserRouter } from 'react-router-dom';
+
+import './index.css';
 
 const theme = createTheme({
   typography: {
@@ -13,8 +15,10 @@ const theme = createTheme({
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
